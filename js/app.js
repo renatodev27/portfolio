bfb80fb773119d70cb1a8aff9157824a871e7f98
Renato Ramos <renatoramos.devops@gmail.com>
2021-09-27 17:22:11 -0500

    Added final design with proyects and links

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function openProyect(destination) {
+    switch (destination) {
+        case 'ecommerce' : window.open('https://renatodev27.github.io/web-ecommerce-un/');
+        case 'cashregis' : window.open('https://renatodev27.github.io/Caja_registradora/');
+        case 'calculadora' : window.open('https://renatodev27.github.io/calculadora/');
+        case 'pokedex' : window.open('https://renatodev27.github.io/pokedex/');
+    }
+}
